ff064c6c6b3b948d22dad5fcd6a5edee2f576a7c
ope1 <ope1_u_pewka@mail.ru>
2014-12-23 18:58:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,17 +5,25 @@ using namespace std;
 int main(void)
 {
 	int n, m;
+	int k;
+	cout << "Size of matrix:" << endl;
 	cin >> n >> m;
 	Matrix p(n, m);
+	cout << "Input first matrix:" << endl;
 	cin >> p;
 	cout << endl;
-	cin >> n >> m;
-	Matrix p1 = p;
+	Matrix p1(n, m);
+	cout << "Input second matrix:" << endl;
 	cin >> p1;
 	cout << endl;
+	cout << "Multiple of matrix:" << endl;
 	cout << p*p1 << endl;
+	cout << "Vichitanie:" << endl;
 	cout << p-p1 << endl;
+	cout << "Slozhenie:" << endl;
 	cout << p+p1 << endl;
+	cout << "Multiple na chislo:" << endl;
+	cout << "Vvedite chislo:" << endl;
 	cin >> k;
 	cout << p*k << endl;
 	system("pause");
